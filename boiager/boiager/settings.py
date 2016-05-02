97cd3ff74fa2bf699a2fdaebb086d27b735914b3
Jordi Martinoy <jordi.martyn1@gmail.com>
2016-05-02 22:33:14 +0200

    set language_code and timezone

--- boiager/boiager/settings.py
+++ boiager/boiager/settings.py
@@ -108,9 +108,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.9/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ca-es'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Madrid'
 
 USE_I18N = True
 
