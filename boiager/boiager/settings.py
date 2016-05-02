220811bcf25f931ff1511fa51ae9d7e5f44bdba6
Jordi Martinoy <jordi.martyn1@gmail.com>
2016-05-02 19:34:38 +0200

    "SUBLIME OP"

--- boiager/boiager/settings.py
+++ boiager/boiager/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'boiager.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'db_boiager',
+        'USER': 'root',
+        'PASSWORD': 'mysqlroot',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
