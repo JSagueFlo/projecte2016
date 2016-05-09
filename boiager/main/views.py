ce647eda5dd149bff297d2af9f35008be2c3297f
Jordi Martinoy <jordi.martyn1@gmail.com>
2016-05-09 22:12:56 +0200

    update

--- boiager/main/views.py
+++ boiager/main/views.py
@@ -29,6 +29,7 @@ def signup(request):
 	if request.method == 'POST':
 		if form.is_valid():
 			user = form.save()
+			
 			auth_login(request, user)
 			return redirect('/')
 	context = {'form': form, 'sliders': sliders}
